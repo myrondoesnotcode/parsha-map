ec818ad05d2c8981e0e8becd20f8675f8320c731
Myron Shneider <myrons@Mac-2.lan>
2026-04-22 21:47:18 +0300

    fix: remove ReactQueryDevtools from production build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import App from './App'
 import { ErrorBoundary } from './components/ErrorBoundary'
@@ -20,7 +19,6 @@ createRoot(document.getElementById('root')!).render(
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ErrorBoundary>
   </StrictMode>
